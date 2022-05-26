e1439bbfb494fc5e5d3c246c3da95976069e5780
lucasg-c <lucasogjc@gmail.com>
2022-05-26 18:09:17 -0300

    Configuração inicial do HeroService.

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -15,17 +16,28 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  heroes = HEROES;
-
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+  
   onSelect(hero: Hero): void
   {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  getHeroes(): void
+  {
+    this.heroes = this.heroService.getHeroes();
+  }
 
-  ngOnInit(): void {
+  constructor
+  (
+    private heroService: HeroService
+  ) 
+  { }
+
+  ngOnInit(): void
+  {
+    this.getHeroes();
   }
 
 }
