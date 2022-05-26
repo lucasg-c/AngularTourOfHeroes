3182a6a3dd2111b19a8cbf9fd11c0a2dc827b975
lucasg-c <lucasogjc@gmail.com>
2022-05-26 19:34:08 -0300

    Codificado o retorno do array de objetos com 'Observable'. Implementado componente 'Messages', exibindo histórico de objetos clicados.

--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  getHeroes(): Hero[]
+  getHeroes(): Observable<Hero[]>
   {
-    return HEROES;
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes.')
+    return heroes;
   }
 
-  constructor() { }
+  constructor
+  (
+    private messageService: MessageService
+  ) 
+  { }
 }
--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -22,16 +22,18 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero): void
   {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected Hero id=${hero.id}`);
   }
 
   getHeroes(): void
   {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   constructor
   (
-    private heroService: HeroService
+    private heroService: HeroService, private messageService: MessageService
   ) 
   { }
 
