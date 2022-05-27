a372d23a70d6fd6430ff46eb2ee3734f3c702647
lucasg-c <lucasogjc@gmail.com>
2022-05-27 16:28:44 -0300

    Adicionadas routes; listagem de objetos 'hero' em um route próprio. Codificado 'nav' e implementado 'dashboard', a página padrão da aplicação quando não há especificação de link.

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -10,20 +10,8 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero =
-  {
-    id: 1,
-    name: 'Windstorm'
-  };
 
   heroes: Hero[] = [];
-  selectedHero?: Hero;
-  
-  onSelect(hero: Hero): void
-  {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected Hero id=${hero.id}`);
-  }
 
   getHeroes(): void
   {
