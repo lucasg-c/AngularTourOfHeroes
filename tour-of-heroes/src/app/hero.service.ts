08ee8d1197698953c4a07931046716d2eb9c5c1f
lucasg-c <lucasogjc@gmail.com>
2022-05-29 14:11:16 -0300

    Implementado método 'updateHero'.

--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -12,6 +12,10 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
   private heroesUrl = 'api/heroes';
+  httpOptions =
+  {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   getHeroes(): Observable<Hero[]>
   {
@@ -33,6 +37,16 @@ export class HeroService {
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
+  }
+
+  updateHero(hero: Hero): Observable<any>
+  {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions)
+      .pipe
+      (
+        tap(_ => this.log(`updated hero id=${hero.id}`)),
+        catchError(this.handleError<any>('updateHero'))
+      )
   }
 
   private log(message: string)
