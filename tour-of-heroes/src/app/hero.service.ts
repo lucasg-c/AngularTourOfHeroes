b24cadfb8e9d232a9fa0c5d630e43cd98027736b
lucasg-c <lucasogjc@gmail.com>
2022-05-29 14:41:30 -0300

    Implementado método 'addHero'.

--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -46,7 +46,17 @@ export class HeroService {
       (
         tap(_ => this.log(`updated hero id=${hero.id}`)),
         catchError(this.handleError<any>('updateHero'))
-      )
+      );
+  }
+
+  addHero(hero: Hero): Observable<Hero>
+  {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
+      .pipe
+      (
+        tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
+      );
   }
 
   private log(message: string)
--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -19,6 +19,14 @@ export class HeroesComponent implements OnInit {
       .subscribe(heroes => this.heroes = heroes);
   }
 
+  add(name: string): void
+  {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => { this.heroes.push(hero) });
+  }
+
   constructor
   (
     private heroService: HeroService, private messageService: MessageService
